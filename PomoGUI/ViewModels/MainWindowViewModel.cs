8ff639333a547583ca6745ea6683548b38db4db9
João Pito <joao.pito@hotmail.com>
2023-05-04 12:02:17 +0100

    Added GUI functionality

---
+++ PomoGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using PomodoroCLI.Timer;
+using System;
+
+namespace PomoGUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        string _clock;
+        public string Clock
+        {
+            get => _clock;
+            set => this.RaiseAndSetIfChanged(ref _clock, value);
+        }
+
+        SessionTimer timer;
+
+        public MainWindowViewModel()
+        {
+            timer = new SessionTimer(new SystemTimer());
+            TimeSpan defaultSessionDuration = TimeSpan.FromMinutes(45);
+            timer.SetDuration(defaultSessionDuration);
+        }
+
+        public void StartSession()
+        {
+            timer.Start();
+        }
+    }
+}
---
+++ PomoGUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace PomoGUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
