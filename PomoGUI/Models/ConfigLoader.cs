82f92e8e4ae1b6e613594a9037922304a195b7c3
João Pito <joao.pito@hotmail.com>
2023-05-21 23:29:35 +0100

    Updated binaries

---
+++ PomoGUI/Models/ConfigLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomoGUI.Models
+{
+    internal class ConfigLoader : IConfigLoader
+    {
+        static ConfigLoader? controllerInstance;
+
+        private ConfigLoader() { }
+
+        public static ConfigLoader GetController()
+        {
+            if(controllerInstance == null)
+                controllerInstance = new ConfigLoader();
+
+            return controllerInstance;
+        }
+
+        public SessionParams LoadWorkSession()
+        {
+            TimeSpan duration = LoadWorkDuration();
+            SessionParams loadedSession = new SessionParams(duration, SessionParams.SessionType.Work);
+            return loadedSession;
+        }
+
+        public SessionParams LoadBreakSession()
+        {
+            TimeSpan duration = LoadBreakDuration();
+            SessionParams loadedSession = new SessionParams(duration, SessionParams.SessionType.Break);
+            return loadedSession;
+        }
+
+        TimeSpan LoadWorkDuration()
+        {
+            return new TimeSpan(0, 0, 10);
+        }
+
+        TimeSpan LoadBreakDuration()
+        {
+            return new TimeSpan(0, 0, 15);
+        }
+
+        public void SaveWorkSession(SessionParams session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveBreakSession(SessionParams session)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PomoGUI/Models/IConfigLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomoGUI.Models
+{
+    public interface IConfigLoader
+    {
+        public SessionParams LoadWorkSession();
+        public SessionParams LoadBreakSession();
+        public void SaveWorkSession(SessionParams session);
+        public void SaveBreakSession(SessionParams session);
+
+
+    }
+}
